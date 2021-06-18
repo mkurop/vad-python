2c29e2a92ff933e97bd67d69dac1154a99dd9e8e
Marcin Kuropatwiński <34960598+mkurop@users.noreply.github.com>
2021-06-18 18:01:11 +0200

    Update vad.py

--- vad.py
+++ vad.py
@@ -11,7 +11,6 @@ import fbe
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath("__file__")),"./noise-tracking-hendriks/"))
-print(sys.path)
 import noise_tracking_hendriks
 
 class vad:
